ab79a0e8521b5902cfbcf089093b4ea6d8b41404
Majd <murad.majd@gmail.com>
2015-05-21 16:46:35 -0700

    Created temp seed file, fixed typo

--- db/migrate/20150521123430_create_relationships.rb
+++ db/migrate/20150521123430_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
       t.integer :follower_id
-      t.integer :follwee_id
+      t.integer :followee_id
 
       t.timestamps
     end
