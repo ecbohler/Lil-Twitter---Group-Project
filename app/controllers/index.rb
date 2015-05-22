48e366e0af35dd8ff38268806b6fd4abc21197f9
Dani Rigsby & tmartyny <pair+danrigs13.tmartyny@devbootcamp.com>
2015-05-22 16:46:04 -0700

    comment out if session until ready to implement, remove comment

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -46,7 +46,7 @@ end
 #-------------Tweet route
 
 post '/users/:user_id/tweet' do #creates tweet and redirects to page to view all tweets made by that user
-  Tweet.create(content: params[:content], user_id: params[:user_id]) if session[:user_id] == params[:user_id]
+  Tweet.create(content: params[:content], user_id: params[:user_id]) # if session[:user_id] == params[:user_id]
   redirect "/users/#{params[:user_id]}"
 end
 
@@ -59,7 +59,7 @@ end
 #-------------Follow route
 post 'users/:user_id/follow' do #sends information to follow  this user - redirects to that users page
   User.find(session[:user_id]).follow User.find(params[:user_id])
- redirect '/users/:user_id'
+  redirect "/users/#{session[:user_id]}"
 end
 
 
