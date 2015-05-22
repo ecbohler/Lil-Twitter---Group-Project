90b848938ea609f8a0ad919d630add6f9f2f2b05
Dani Rigsby & tmartyny <pair+danrigs13.tmartyny@devbootcamp.com>
2015-05-22 16:15:12 -0700

    modify routes, add erbs, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@ bob = User.create(handle: "bob")
 susie = User.create(handle: "susie")
 
 Relationship.create(follower_id: bob.id, followee_id: susie.id)
+
+
+tweet1 = Tweet.create(content: "tweet1", user_id: bob.id)
+tweet2 = Tweet.create(content: "tweet2", user_id: susie.id)
