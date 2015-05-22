4a4782e0aa2080084b58d77a83b991ba2f330e13
Dani Rigsby & ecbohler <pair+danrigs13.ecbohler@devbootcamp.com>
2015-05-21 17:16:01 -0700

    Added login feature

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,43 @@
+get '/' do
+  # render home page
+ #TODO: Show all users if user is signed in
+  erb :sign_in_view
+end
+
+#----------- SESSIONS -----------
+
+get '/sessions/new' do
+  erb :sign_in_view
+end
+
+get '/sessions/sign_in/error' do
+  erb :sign_in_error_view
+end
+
+post '/sessions' do
+  if params[:handle]
+    @user = Login.find_by(handle: params[:handle])
+    if @user && @user.password_hash == (params[:password])
+    #   # Should redirect to the user's profile page
+      session[:id] = @user.id
+      redirect '/'
+    else
+      redirect '/sessions/sign_in/error'
+    end
+  end
+end
+
+delete '/sessions/:id' do
+  # sign-out -- invoked
+end
+
+#----------- USERS -----------
+
+get '/users/new' do
+  erb :sign_up_view
+end
+
+post '/users' do
+  @user = Login.create(handle: params[:handle], password_hash: params[:password])
+  redirect '/users/new'
+end
